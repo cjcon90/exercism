631e7b3af9538db8a8721bd127b6ef1fe04208e6
Ciaran Concannon <cjcon90@pm.me>
2021-08-25 22:30:04 +0100

    add solution for twelve days

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,28 +1,31 @@
 from typing import List
 
-LYRICS = {
-	1: ['first', "a Partridge in a Pear Tree."],
-	2: ['second', "two Turtle Doves, "],
-	3: ['third', "three French Hens, "],
-    4: ['fourth', "four Calling Birds, "],
-    5: ['fifth', "five Gold Rings, "],
-    6: ['sixth', "six Geese-a-Laying, "],
-    7: ['seventh', "seven Swans-a-Swimming, "],
-    8: ['eighth', "eight Maids-a-Milking, "],
-    9: ['ninth', "nine Ladies Dancing, "],
-    10: ['tenth', "ten Lords-a-Leaping, "],
-    11: ['eleventh', "eleven Pipers Piping, "],
-    12: ['twelfth', "twelve Drummers Drumming, "]
-}
+LYRICS = ( 
+    ['first', "a Partridge in a Pear Tree."],
+    ['second', "two Turtle Doves, and "],
+    ['third', "three French Hens, "],
+    ['fourth', "four Calling Birds, "],
+    ['fifth', "five Gold Rings, "],
+    ['sixth', "six Geese-a-Laying, "],
+    ['seventh', "seven Swans-a-Swimming, "],
+    ['eighth', "eight Maids-a-Milking, "],
+    ['ninth', "nine Ladies Dancing, "],
+    ['tenth', "ten Lords-a-Leaping, "],
+    ['eleventh', "eleven Pipers Piping, "],
+    ['twelfth', "twelve Drummers Drumming, "]
+)
 
-def recite(start_verse: int, end_verse: int) -> List[str]:
+def recite(start_day: int, end_day: int) -> List[str]:
     song = []
-    while start_verse <= end_verse:
-        verse = f"On the {LYRICS[start_verse][0]} day of Christmas my true love gave to me: "
-        for i in range(start_verse, 0, -1):
-            if i == 1 and start_verse > 1:
-                verse += "and "
-            verse += LYRICS[i][1]
+    while start_day <= end_day:
+        verse = get_verse(start_day - 1)
         song.append(verse)
-        start_verse += 1
+        start_day += 1
     return song
+
+def get_verse(day: int) -> str:
+    verse = f"On the {LYRICS[day][0]} day of Christmas my true love gave to me: "
+    while day >= 0:
+        verse += LYRICS[day][1]
+        day -= 1
+    return verse
